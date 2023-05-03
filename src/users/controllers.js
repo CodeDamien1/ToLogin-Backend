4c9fdd3f353edd0b03d46a42b5eaa05082526839
damien lewis <damien_lewis2k12@hotmail.com>
2023-05-03 14:02:02 +0100

    updated login function

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -14,8 +14,35 @@ const registerUser = async (req, res) => {
     }
 }
 
+const login = async (req, res) => {
+    try {
+        if (req.authUser) {
+            res.status(200).json({
+              message: "success",
+              user: {
+                username: req.authUser.username,
+                email: req.authUser.email
+              }
+            })
+            return
+        }
+        const token = await jwt.sign({id: req.user.id }, process.env.SECRET);
+
+        res.status(200).json({
+            message: "success",
+            user: {
+                username: req.user.username,
+                email: req.user.email,
+                token: token
+            }
+        })
+    } catch (error) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+}
 
 
 module.exports = {
     registerUser,
+    login
 }
