4439825d054b210bb980411853469601e8e91a01
damien lewis <damien_lewis2k12@hotmail.com>
2023-05-03 14:34:01 +0100

    readded login, disappeared somehow

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken")
 const registerUser = async (req, res) => {
     try { 
         const user = await User.create(req.body)
-        const token = await jwt.sign({id: user.id }, process.env.SECRET);
+        const token = await jwt.sign({id: user.id }, process.env.SECRET_KEY);
         res.status(201).json({
             message: "success",
             user: {username: req.body.username, email: req.body.email, token: token}
@@ -14,8 +14,35 @@ const registerUser = async (req, res) => {
     }
 }
 
+const login = async (req, res) => {
+    try {
+        if (req.authUser) {
+            res.status(200).json({
+              message: "success",
+              user: {
+                username: req.authUser.username,
+                email: req.authUser.email
+              }
+            })
+            return
+        }
+        const token = await jwt.sign({id: req.user.id }, process.env.SECRET_KEY);
+
+        res.status(200).json({
+            message: "success",
+            user: {
+                username: req.user.username,
+                email: req.user.email,
+                token: token
+            }
+        })
+    } catch (error) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+}
 
 
 module.exports = {
     registerUser,
+    login
 }
