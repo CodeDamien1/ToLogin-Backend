2443fe3cd881ad9ab39eaa9670adceddff13e385
Ray Merrell <RayMerrell@outlook.com>
2023-05-04 16:30:51 +0100

    bugfix

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -5,6 +5,7 @@ const jwt = require("jsonwebtoken");
 
 const comparePass = async (req, res, next) => {
   try {
+    console.log("compare");
     //lookup user in database using username sent in request body
     req.ourUser = await User.findOne({
       where: { userName: req.body.username },
@@ -15,7 +16,8 @@ const comparePass = async (req, res, next) => {
     }
     
     //use bcrypt to compare the incoming password with the encrypted one stored in the database
-
+console.log("passwords", req.body.password,
+req.ourUser.password);
     req.ourUser.passed = await bcrypt.compare(
       req.body.password,
       req.ourUser.password
@@ -23,6 +25,7 @@ const comparePass = async (req, res, next) => {
     //configure return object
     if (req.ourUser.passed) {
       req.user = {
+        id:req.ourUser.id,
         username: req.ourUser.username,
         password: req.ourUser.password,
       };
@@ -37,11 +40,10 @@ const comparePass = async (req, res, next) => {
 
 const hashPass = async (req, res, next) => {
   try {
+    
+    console.log("hash");
     //get number of salt rounds from .env file
-
-
-    const saltRounds = process.env(SALT_ROUNDS);
-
+    const saltRounds = process.env.SALT_ROUNDS;
     //get user password and pass to bcrypt to create the hash
     req.body.password = await bcrypt.hash(
       req.body.password,
@@ -59,21 +61,25 @@ const hashPass = async (req, res, next) => {
 const tokenCheck = async (req, res, next) => {
 
   try {
+    
+    console.log("token check");
     //check for the authorization header
     if (!req.header("Authorization")) {
       throw new Error("Missing Credentials");
     }
     //extract the token from the request header
-    const token = req.header("Authorization").replace("Bearer", "");
+    const token = req.header("Authorization").replace("Bearer ", "");
+    console.log("token", token);
     //decode the token
     const newID = jwt.verify(token, process.env.SECRET_KEY);
+    console.log("***newID", newID.id);
     //lookup user from decoded id
-    const newUser = User.findOne({ where: { id: newID } });
+    const newUser = await User.findOne({ where: { id:newID.id } });
+    console.log("newUser", newUser);
     //check if we found a user and return with response if not
     if (!newUser) {
-
+      console.log(("No user found"));
       res.status(401).json({ messge: "User not authorised" });
-
       return;
     }
     //prepare successful response
@@ -82,12 +88,11 @@ const tokenCheck = async (req, res, next) => {
       username: newUser.username,
       password: newUser.password,
     };
+    console.log("req.authCheck", req.authCheck);
     //go to the next stage in the route
     next();
   } catch (error) {
-
     console.error(error)
-
     res.status(501).json({ message: "failure", error: error });
   }
 };
